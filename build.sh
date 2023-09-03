11a76b59f59a8f79f56f8da98314f4111a5535a1
Roland Hermans <rolandh@users.sourceforge.net>
2023-09-03 18:15:13 +0200

    Use CMake option --toolchain

--- build.sh
+++ build.sh
@@ -16,10 +16,10 @@ cmake --build build
 
 if [[ $1 = cross ]] ; then
   # build using 32-bit MinGW-w64 GCC cross-compiler
-  cmake -S . -B build-win32 -D CMAKE_TOOLCHAIN_FILE=CMake/i686-w64-mingw32.cmake -D MyGen_DIR=build
+  cmake -S . -B build-win32 --toolchain CMake/i686-w64-mingw32.cmake -D MyGen_DIR=build
   cmake --build build-win32
 
   # build using 64-bit MinGW-w64 GCC cross-compiler
-  cmake -S . -B build-win64 -D CMAKE_TOOLCHAIN_FILE=CMake/x86_64-w64-mingw32.cmake -D MyGen_DIR=build
+  cmake -S . -B build-win64 --toolchain CMake/x86_64-w64-mingw32.cmake -D MyGen_DIR=build
   cmake --build build-win64
 fi
