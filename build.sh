3e7f33ba9f486ecb58c302263fe1d74ebda79c3d
Roland Hermans <rolandh@users.sourceforge.net>
2023-09-03 18:13:19 +0200

    Run make via CMake

--- build.sh
+++ build.sh
@@ -12,20 +12,14 @@ fi
 
 # build using native compiler
 cmake -S . -B build
-cd build
-make
-cd ..
+cmake --build build
 
 if [[ $1 = cross ]] ; then
   # build using 32-bit MinGW-w64 GCC cross-compiler
   cmake -S . -B build-win32 -D CMAKE_TOOLCHAIN_FILE=CMake/i686-w64-mingw32.cmake -D MyGen_DIR=build
-  cd build-win32
-  make
-  cd ..
+  cmake --build build-win32
 
   # build using 64-bit MinGW-w64 GCC cross-compiler
   cmake -S . -B build-win64 -D CMAKE_TOOLCHAIN_FILE=CMake/x86_64-w64-mingw32.cmake -D MyGen_DIR=build
-  cd build-win64
-  make
-  cd ..
+  cmake --build build-win64
 fi
